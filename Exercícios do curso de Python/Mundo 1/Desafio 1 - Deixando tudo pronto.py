a1f9867a8575357c833f2d817e9a9bcdfdd3bdb0
ALBEDERENG <albedereng@gmail.com>
2025-07-30 11:33:46 -0300

    Add files via upload

---
+++ Exercícios do curso de Python/Mundo 1/Desafio 1 - Deixando tudo pronto.py
@@ -0,0 +1,4 @@
+import os
+import math
+os.system('cls')
+print("Olá, mundo!")
---
+++ Exercícios do curso de Python/Mundo 1/Desafio 2 - Respondendo ao Usuário.py
@@ -0,0 +1,5 @@
+import os
+import math
+os.system('cls')
+n = str(input("Digite seu nome: "))
+print("É um prazer te conhecer, {}!".format(n))
